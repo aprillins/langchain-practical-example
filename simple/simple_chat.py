50016a1dcac9c36391eeda3a68547da126cf059b
aprillins <aprillins@gmail.com>
2023-10-24 08:33:18 +0700

    Work in progress 2023-10-24 08:33:18

---
+++ simple/simple_chat.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.schema.output_parser import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
+model = ChatOpenAI(model="gpt-4o-mini")
+parser = StrOutputParser()
+
+messages = [
+    SystemMessage("you are a mathematician you calculate number of character in a sentence"),
+    HumanMessage("How many characters in this sentence?")
+]
+
+result = model.invoke(messages)
+print(parser.invoke(result))
